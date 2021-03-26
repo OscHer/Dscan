b8fd83a229bff906a0e072148f70c6c7a99a6b61
oscher <oscarherediallorens@yahoo.es>
2021-03-26 16:10:50 +0100

    Create main script with algorithm skeleton

---
+++ dscan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Main script for dscan project.
+# More info: https://github.com/OscHer/Dscan
+
+# Variables section
+# Functions
+  # Check args
+  # Scan one host. Input: valid ip address. Output: array with ssl/tls info
+  # Add one register to mysql
+# Main
+  # Check args or exit
+  
