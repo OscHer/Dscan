88d8e03153bc5cf7f7b8e0afe2ce871228799cb2
oscher <oscarherediallorens@yahoo.es>
2021-03-28 12:26:46 +0200

    Generate sslscan result array

--- dscan.sh
+++ dscan.sh
@@ -5,13 +5,14 @@
 #  sslscan: Protocol scanner.
 
 # Variables section
+declare -A scanResultArray # Iterator array to get protocol=state as pair=value as a result of scanned HOST:PORT
 # Binaries
 SSLSCAN_BIN=$(which sslscan) # Get sslscan full path. #IMPROV: This could be abstracted to use any other scan utility since detection and parsing are fully parametrizable.
 SSLSCAN_OPTIONS="--no-check-certificate --no-cipher-details --no-sigs --no-ciphersuites --no-fallback  --no-renegotiation --no-groups --no-heartbleed --no-compression"
 # Inventory file
 INVENTORY='fichero.txt' # Harcoded for requirements of the test. #IMPROV Should be just a default and overwritten with params '-f' or the like
 # Debug
-DEBUG=1 # Set to 1 for verbose output. # IMPROV: intialize if '-v' when launch
+DEBUG=0 # Set to 1 for verbose output. # IMPROV: intialize if '-v' when launch
 # DB section
 MYSQL_HOSTNAME='localhost' # Harcoded since it's a local connection
 #MYSQL_USERNAME='' # Default container username. May change later
@@ -26,14 +27,26 @@ check_ip()
   ip route get $IP > /dev/null 2>&1 ; echo $?
 }
 
+
+
 # Scan one host. Input: valid ip address. Output: array with ssl/tls info
 scan_one_host ()
 {
-  HOST=$1 # TODO: This seems redundant but later we can add resolving strategies if hostname provided 
+  HOST=$1 # IMPROV: This seems redundant but later we can add resolving strategies if hostname provided 
+  PORT=$2 # Port we'll scan for SSL negotiation
 
-  [[ $DEBUG -eq 1 ]] && echo ${FUNCNAME[0]}: $HOST # Debug mode
-  $SSLSCAN_BIN $SSLSCAN_OPTIONS $HOST:80 | grep abled
+  [[ $DEBUG -eq 1 ]] && echo ${FUNCNAME[0]}: $HOST:$PORT # Debug mode
 
+  # Scan host:port and format output into an array
+  while IFS='\n' read -r line; do
+    CLAVE="$(echo $line | cut -d' ' -f1 | tr -d '.')"
+    VALOR="$(echo $line | cut -d' ' -f2)"
+    scanResultArray[$CLAVE]=$VALOR
+
+    [[ $DEBUG -eq 1 ]] && echo $HOST'--'$CLAVE'---'$VALOR'--' # Debug purpose
+  done < <($SSLSCAN_BIN $SSLSCAN_OPTIONS $HOST:$PORT 2>/dev/null | grep abled | tr -s " ")
+
+  [[ $DEBUG -eq 1 ]] && for CLAVE in "${!scanResultArray[@]}"; do echo "$CLAVE"'='${scanResultArray[$CLAVE]}; done
 }
 
 # Main function: highest level of abstraction of this algorithm.
@@ -46,12 +59,17 @@ main()
 
   # Loop over inventory file
   for SERVER in $(cat $INVENTORY); do
-    # Check for valid IP or jump 
-      if [[ $(check_ip $SERVER) -eq 0 ]] 
-        then
-          echo "Scanning $SERVER"      # Scan host port 443
-        # If port reachable 
-          # Insert to ddbb
+      # Check for valid IP or jump 
+      if [[ $(check_ip $SERVER) -eq 0 ]]; then
+          echo "Scanning $SERVER:"
+          scan_one_host $SERVER 443
+           for CLAVE in "${!scanResultArray[@]}"; do echo "$CLAVE"'='${scanResultArray[$CLAVE]}; done
+
+          #if [[ ${#globalarray}SCAN_RETURN_VALUE -eq 0 ]]; then # Scan host port 443 # IMPROV: provide an array of ports to iterate it instead of hardcoding
+          #  [[ $DEBUG -eq 1 ]] && echo "Salida de scan: $SCAN_RETURN_VALUE"
+          #else
+          #  echo "Host $SERVER:$PORT unreachable. Skipping"
+          #fi
       # Scan host port 8443
         # If port reachable
           # Insert to ddbb   
